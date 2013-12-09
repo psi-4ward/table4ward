e6812e01406b5844af7ce7f3ff5ba3fe0da56ecb
Christoph Wiechert <wio@psitrax.de>
2013-12-09 13:30:00 +0100

    Fixed: autoload-creator overwrites autoload.php

--- config/autoload.php
+++ config/autoload.php
@@ -1,17 +1,6 @@
 <?php
 
 /**
- * Contao Open Source CMS
- *
- * Copyright (c) 2005-2013 Leo Feyer
- *
- * @package Table4ward
- * @link    https://contao.org
- * @license http://www.gnu.org/licenses/lgpl-3.0.html LGPL
- */
-
-
-/**
  * Register the classes
  */
 ClassLoader::addClasses(array
